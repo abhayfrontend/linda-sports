e160b2c382574da83facbd2905eed27af9d7f9b3
abhayfrontend <46017583+abhayfrontend@users.noreply.github.com>
2020-05-24 01:22:06 +0530

    pushes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() {
-    window.alert('website is under maintenance')
+  
    }
 
   ngOnInit() {
